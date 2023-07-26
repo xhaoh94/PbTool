208f22c7e8ecaf343788e2c64b38a1a1a6073e07
xhaoh94 <xhaoh94@qq.com>
2023-07-26 14:11:17 +0800

    提交

--- PbTool/bin/Debug/net6.0/out/ts/BCST.ts
+++
@@ -1,8 +0,0 @@
-﻿//自动生成的代码，请勿修改!!!
-export namespace Pb{
-	export const enum BCST{
-		Bcst_EntityEnter = 30001,
-		Bcst_EntityLeave = 30002,
-		Bcst_EntityMove = 30003,
-	}
-}
--- PbTool/bin/Debug/net6.0/out/ts/Dpdts.ts
+++
@@ -1,9 +0,0 @@
-﻿//自动生成的代码，请勿修改!!!
-export namespace Pb{
-	export interface Dpdts{
-		csToMessage?: { [key: number]: string };
-		scToMessage?: { [key: number]: string };
-		bcstToMessage?: { [key: number]: string };
-		messageData?: { [key: string]: string[][] };
-	}
-}
